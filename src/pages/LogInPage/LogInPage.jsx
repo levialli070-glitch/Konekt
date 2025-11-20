a05410482ce25a7995872da0b342c257c452fb32
Zerolight0 <Treasuregeorge75@gmail.com>
2025-11-20 21:47:30 +0100

    feat: enhance button styles with hover effects and back navigation

--- src/pages/LogInPage/LogInPage.jsx
+++ src/pages/LogInPage/LogInPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { MdArrowBack } from "react-icons/md";
 import classes from "./LogInPage.module.css";
 import LogInForm from "../../components/LogInForm/LogInForm";
 
 const LogInPage = () => {
   return (
     <div className={classes.container}>
+      <Link to="/" className={classes.backBtn}>
+        <MdArrowBack />
+      </Link>
       <div className={classes.orb1}></div>
       <div className={classes.orb2}></div>
       <div className={classes.orb3}></div>
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { MdArrowBack } from "react-icons/md";
 import classes from "./SignUpPage.module.css";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 
 const SignUpPage = () => {
   return (
     <div className={classes.container}>
+      <Link to="/" className={classes.backBtn}>
+        <MdArrowBack />
+      </Link>
       <div className={classes.orb1}></div>
       <div className={classes.orb2}></div>
       <div className={classes.orb3}></div>
